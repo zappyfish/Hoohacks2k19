5ec070e6535e300bb8aa48fae4664a3166d64d85
zappyfish <liamkelly2015@gmail.com>
2019-03-02 12:52:47 -0500

    size

--- SyllaSnap/app/src/main/java/com/example/syllasnap/data/OCRResponse.java
+++ SyllaSnap/app/src/main/java/com/example/syllasnap/data/OCRResponse.java
@@ -11,7 +11,11 @@ public class OCRResponse {
         mOCRDataList = new ArrayList<>();
     }
 
-    public OCRData getText(int pos) {
+    public OCRData getData(int pos) {
         return mOCRDataList.get(pos);
+    }
+
+    public int getNumData() {
+        return mOCRDataList.size();
     }
 }
