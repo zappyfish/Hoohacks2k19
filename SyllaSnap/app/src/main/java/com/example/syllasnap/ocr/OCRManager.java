5dbec58c5f32cf8204b1495a65b41f9f435608e0
Anna Ouyang <annaouyang@Annas-MacBook-Pro.local>
2019-03-02 17:37:21 -0500

    bloop

--- SyllaSnap/app/src/main/java/com/example/syllasnap/ocr/OCRManager.java
+++ SyllaSnap/app/src/main/java/com/example/syllasnap/ocr/OCRManager.java
@@ -1,8 +1,20 @@
 package com.example.syllasnap.ocr;
 
-import android.graphics.Bitmap;
+import com.example.syllasnap.data.OCRData;
+import com.example.syllasnap.data.OCRResponse;
+import com.google.cloud.vision.v1.AnnotateImageRequest;
+import com.google.cloud.vision.v1.AnnotateImageResponse;
+import com.google.cloud.vision.v1.BatchAnnotateImagesResponse;
+import com.google.cloud.vision.v1.EntityAnnotation;
+import com.google.cloud.vision.v1.Feature;
+import com.google.cloud.vision.v1.Feature.Type;
+import com.google.cloud.vision.v1.Image;
+import com.google.cloud.vision.v1.ImageAnnotatorClient;
+import com.google.cloud.vision.v1.Vertex;
+import com.google.protobuf.ByteString;
 
-import java.io.File;
+import java.util.*;
+
 
 public class OCRManager {
 
@@ -20,26 +32,49 @@ public class OCRManager {
         return sInstance;
     }
 
-    // TODO: Choose one of the three to implement, whatever is easiest, I'm not sure.
-    // Useful links:
-    // https://cloud.google.com/vision/docs/libraries#client-libraries-install-java
+    /**
+     * makeOCRRequest(byte[] syllabusData)
+     *
+     * @param syllabusData - a byte[] containing the image of syllabus
+     * @return OCRRequest ... containg
+     */
+    public OCRResponse makeOCRRequest(byte[] syllabusData) throws Exception {
+        OCRResponse requestResult = new OCRResponse();
+        List<AnnotateImageRequest> requests = new ArrayList<>();
+        ByteString imgBytes = ByteString.copyFrom(syllabusData);
 
+        Image img = Image.newBuilder().setContent(imgBytes).build();
+        Feature feat = Feature.newBuilder().setType(Type.TEXT_DETECTION).build();
+        AnnotateImageRequest request = AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build();
 
-    // TODO: Complete me (don't worry about the return type, I'll write a callback) assuming vision
-    // API is async
-    public void makeOCRRequest(Bitmap syllabusImage) {
+        try (ImageAnnotatorClient client = ImageAnnotatorClient.create()) {
+            BatchAnnotateImagesResponse response = client.batchAnnotateImages(requests);
+            List<AnnotateImageResponse> responses = response.getResponsesList();
+            requests.add(request);
 
-    }
+            for (AnnotateImageResponse res : responses) {
+                if (res.hasError()) {
+                    return null;
+                }
+                for (EntityAnnotation annotation : res.getTextAnnotationsList()) {
+                    if (annotation.getDescription() != null) {
+                        String text = annotation.getDescription();
+                        List<Vertex> vertices = annotation.getBoundingPoly().getVerticesList();
 
-    // TODO: Complete me (don't worry about the return type, I'll write a callback) assuming vision
-    // API is async
-    public void makeOCRRequest(File syllabusImageFile) {
+                        int left = vertices.get(0).getX();
+                        int top = vertices.get(1).getY();
+                        int right = vertices.get(2).getX();
+                        int bottom = vertices.get(3).getY();
 
-    }
+                        OCRData data = new OCRData(text, left, top, right, bottom);
+                        requestResult.addData(data);
 
-    // TODO: Complete me (don't worry about the return type, I'll write a callback) assuming vision
-    // API is async
-    public void makeOCRRequest(String syllabusImageFilePath) {
+                    }
+                }
+            }
 
+        }
+
+        return requestResult;
     }
 }
