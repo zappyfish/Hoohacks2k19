b230cab91951259fb84bc8c41cea22068ae5a8dd
Angel Martinez <amartinez10494@gmail.com>
2019-03-03 09:50:18 -0500

    updates

--- SyllaSnap/app/src/main/java/com/example/syllasnap/MainActivity.java
+++ SyllaSnap/app/src/main/java/com/example/syllasnap/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.syllasnap;
 
+import android.app.Activity;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.Button;
 
 import com.example.syllasnap.camera.CameraActivity;
@@ -13,6 +15,8 @@ import com.example.syllasnap.test_activities.MaraTestActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private WebView mWebView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,9 +24,11 @@ public class MainActivity extends AppCompatActivity {
         setUpTestActivityButton(R.id.start_camera, new Intent(MainActivity.this, CameraActivity.class));
 
         // startActivity(new Intent(MainActivity.this, AuthActivity.class));
+        WebView webView = (WebView) findViewById(R.id.webview);
+            webView.loadUrl( "https://env-132697.customer.cloud.microstrategy.com:443/MicroStrategy/servlet/mstrWeb?evt=3186&src=mstrWeb.3186&subscriptionID=9027B4B011E93D74DEE30080EF0579A1&Server=ENV-132697LAIOUSE1&Project=MicroStrategy%20Tutorial&Port=0&share=1&hiddensections=header,path,dockTop,dockLeft,footer");
+
     }
-
-
+    
     private void setUpTestActivityButton(int btnId, final Intent startIntent) {
         Button button = (Button) findViewById(btnId);
         button.setOnClickListener(new View.OnClickListener() {
